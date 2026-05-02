705db92431a7b0df29c7e117f2a7f77e2a8ee657
Bastian Eggum Huuse <bastian.eggum.huuse@gmail.com>
2026-05-02 15:17:26 +0200

    Double camera functionality

--- back_end/camera.js
+++ back_end/camera.js
@@ -1,17 +1,37 @@
-let viewfinder = document.getElementById("viewfinder")
-let snapshot = document.getElementById("snapshot")
+let viewfinder_e = document.getElementById("viewfinder_environment")
+let viewfinder_u = document.getElementById("viewfinder_user")
+let snapshot_e 	 = document.getElementById("snapshot_environment")
+let snapshot_u 	 = document.getElementById("snapshot_user")
 
 async function camera_init() {
 	// Declare video stream
-	let stream = null
+	let environment_stream = null
+	let user_stream = null
+
+	let environment_constraints = {
+		audio: false,
+		video: {
+			facingMode: "environment"
+		}
+	}
+
+	let user_constraints = {
+		audio: false,
+		video: {
+			facingMode: "user" // for front facing mode
+		}
+	}
 
 	try {
 		// Get video stream from the navigator
-		stream = await navigator.mediaDevices.getUserMedia({video:true, audio:false});
+		environment_stream = await navigator.mediaDevices.getUserMedia(environment_constraints);
+		user_stream = await navigator.mediaDevices.getUserMedia(user_constraints);
 
 		// Link video stream to the viewfinder, and play stream
-		viewfinder.srcObject = stream;
-		viewfinder.play();
+		viewfinder_e.srcObject = environment_stream;
+		viewfinder_e.play();
+		viewfinder_u.srcObject = user_stream;
+		viewfinder_u.play();
 	} catch(error) {
 		document.getElementById("header").innerHTML = "Error starting camera..";
 	}
@@ -20,8 +40,10 @@ async function camera_init() {
 async function camera_shutter() {
 
 	// Draw the image currently in the viewfinder onto the canvas
-	var context = snapshot.getContext("2d");
-	context.drawImage(viewfinder,0,0,640,480);
+	var context = snapshot_e.getContext("2d");
+	context.drawImage(viewfinder_e,0,0,640,480);
+	var context = snapshot_u.getContext("2d");
+	context.drawImage(viewfinder_u,0,0,640,480);
 
 	save_image();
 }
