2b750ab8409bf3e3689b67f7f161339b8b1090e5
Bastian Eggum Huuse <bastian.eggum.huuse@gmail.com>
2026-05-02 12:38:56 +0200

    Working viewfinder

--- main.js
+++ main.js
@@ -1,19 +1,14 @@
 let viewfinder = document.getElementById("viewfinder")
-let button_start_camera = document.getElementById("start_camera")
 
-// starting camera
-button_start_camera.addEventListener("click", async function() {
-	document.getElementById("header").innerHTML = "Starting camera..";
-
-	let stream = null;
+async function init_camera() {
+	let stream = null
 	try {
-		stream = await navigator.mediaDevices.getUserMedia(
-			{video : true, audio: false}
-		);
+		stream = await navigator.mediaDevices.getUserMedia({video:true, audio:false});
+		viewfinder.srcObject = stream;
+		viewfinder.play();
 	} catch(error) {
 		document.getElementById("header").innerHTML = "Error starting camera..";
 	}
 
-	viewfinder.srcObject = stream;
-	viewfinder.style.display = "block";
-});
+	document.getElementById("header").innerHTML = "Camera started";
+}
