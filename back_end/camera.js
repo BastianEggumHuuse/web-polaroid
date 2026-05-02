df99a8c8929dd4e1e4785d2b5ea22c8a92f41caa
Bastian Eggum Huuse <bastian.eggum.huuse@gmail.com>
2026-05-02 16:14:44 +0200

    Zoom in viewframe (not canvas)

--- back_end/camera.js
+++ back_end/camera.js
@@ -2,7 +2,10 @@ let viewfinder_e = document.getElementById("viewfinder_environment")
 let viewfinder_u = document.getElementById("viewfinder_user")
 let snapshot_e 	 = document.getElementById("snapshot_environment")
 let snapshot_u 	 = document.getElementById("snapshot_user")
+let stage_e 	 = document.getElementById("stage_environment")
+let stage_u 	 = document.getElementById("stage_user")
 
+// Base camera functionality
 async function camera_init() {
 	// Declare video stream
 	let environment_stream = null
@@ -37,17 +40,65 @@ async function camera_init() {
 	}
 }
 
+// Shutter and saving functionality
 async function camera_shutter() {
 
 	// Draw the image currently in the viewfinder onto the canvas
 	var context = snapshot_e.getContext("2d");
-	context.drawImage(viewfinder_e,0,0,640,480);
+	context.drawImage(stage_e,0,0,640,480);
 	var context = snapshot_u.getContext("2d");
-	context.drawImage(viewfinder_u,0,0,640,480);
+	context.drawImage(stage_u,0,0,640,480);
 
 	save_image();
 }
 
 function save_image() {
 	// Nothing for now
+}
+
+// Zooming functionality
+let zoom = 1.0;
+let zoom_min = 1.0;
+let zoom_max = 2.0;
+let zoom_step = 0.1;
+
+function zoom_in(){
+	zoom = clamp(zoom_min,zoom_max,zoom + zoom_step);
+	update_zoom();
+}
+
+function zoom_out(){
+	zoom = clamp(zoom_min,zoom_max,zoom - zoom_step);
+	update_zoom();
+}
+function update_zoom()
+{
+	
+	// Which transform is used depends on browser 
+	var properties = ['transform', 'WebkitTransform', 'MozTransform','msTransform', 'OTransform'];
+	let prop = properties[0]
+
+	// I just kinda stole this code from mozilla bro
+	/* Ok so this is probably necessary but it doesn't work!!
+	// Find out which CSS transform the browser supports
+	for(i=0,j=properties.length;i<j;i++){
+		if(typeof stage.style[properties[i]] !== 'undefined'){
+			prop = properties[i];
+			break;
+		}
+	}*/
+
+	// Actually applying zoom
+	viewfinder_e.style[prop] = "scale("+zoom+")";
+}
+
+// Other functions
+function clamp(a,b,t){
+	if (t < a){
+		return a;
+	}
+	if (t > b){
+		return b;
+	}
+	return t;
 }
